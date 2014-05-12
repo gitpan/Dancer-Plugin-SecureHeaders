175e30d7027307c5c0c8f94a863d4d5e895be6ed
Colin Ewen <draecas@gmail.com>
2014-05-12 13:01:15 +0000

    Import of CASAO/Dancer-Plugin-SecureHeaders-1.0.3 from CPAN.
    gitpan-cpan-distribution: Dancer-Plugin-SecureHeaders
    gitpan-cpan-version:      1.0.3
    gitpan-cpan-path:         CASAO/Dancer-Plugin-SecureHeaders-1.0.3.tar.gz
    gitpan-cpan-author:       CASAO
    gitpan-cpan-maturity:     released

--- lib/Dancer/Plugin/SecureHeaders.pm
+++ lib/Dancer/Plugin/SecureHeaders.pm
@@ -12,7 +12,7 @@ Dancer::Plugin::SecureHeaders - Automate HTTP Security headers.
 
 =head1 VERSION
 
-Version 1.0.2
+Version 1.0.3
 
 =cut
 
@@ -20,7 +20,7 @@ Version 1.0.2
 
 =cut
 
-our $VERSION = '1.0.2';
+our $VERSION = '1.0.3';
 
 =head1 DESCRIPTION
 
